e4d85122d094b7cbc5a32cde8e8c233cd7c830da
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-09 18:14:03 +0900

    [Add] 의류 쇼케이스 추가.

--- src/pages/ShopMain/ShopMainShowcase.jsx
+++ src/pages/ShopMain/ShopMainShowcase.jsx
@@ -1,4 +1,4 @@
-import {ShopMainShowcaseContainer} from "./styles";
+import {ShopMainShowcaseContainer, ShowcaseItemDivider} from "./styles";
 import ShowcaseItem from "./ShowcaseItem";
 
 const ShopMainShowcase = () => {
@@ -8,6 +8,10 @@ const ShopMainShowcase = () => {
         <ShowcaseItem image={"ModelX.png"} title={"Model X 액세서리"}/>
         <ShowcaseItem image={"ModelY.png"} title={"Model Y 액세서리"} isTitleDark={true}/>
         <ShowcaseItem image={"Charging.png"} title={"충전"}/>
+        <ShowcaseItemDivider>
+            <ShowcaseItem image={"Men.png"} title={"남성 의류"} isTitleDark={true} isHalfWidth={true}/>
+            <ShowcaseItem image={"Women.png"} title={"여성 의류"} isTitleDark={true} isHalfWidth={true}/>
+        </ShowcaseItemDivider>
     </ShopMainShowcaseContainer>
 }
 
