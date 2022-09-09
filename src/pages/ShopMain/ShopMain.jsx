321ac8535cc39bdd2f4340eec30547a0fc21cd05
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-09 18:26:10 +0900

    [Add] 샵푸터 추가.

--- src/pages/ShopMain/ShopMain.jsx
+++ src/pages/ShopMain/ShopMain.jsx
@@ -1,11 +1,13 @@
 import {ShopMainContainer} from "./styles";
 import ShopMainCarousel from "./ShopMainCarousel";
 import ShopMainShowcase from "./ShopMainShowcase";
+import ShopFooter from "../../components/ShopFooter/ShopFooter";
 
 const ShopMain = () => {
     return <ShopMainContainer>
         <ShopMainCarousel/>
         <ShopMainShowcase/>
+        <ShopFooter/>
     </ShopMainContainer>
 }
 
