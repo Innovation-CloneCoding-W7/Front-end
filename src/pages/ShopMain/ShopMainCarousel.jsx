ad90306bd9104f4b585aa7c542483b1caf1f8ce6
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-09 16:27:17 +0900

    [Add] 슬라이더 오버레이 추가.

--- src/pages/ShopMain/ShopMainCarousel.jsx
+++ src/pages/ShopMain/ShopMainCarousel.jsx
@@ -1,4 +1,4 @@
-import {ShopMainCarouselContainer} from "./styles";
+import {ShopMainCarouselContainer, SwiperOverlay} from "./styles";
 import {Autoplay, Pagination, Scrollbar} from "swiper";
 import {Swiper, SwiperSlide} from "swiper/react";
 import "swiper/css";
@@ -20,9 +20,19 @@ const ShopMainCarousel = () => {
         >
             <SwiperSlide>
                 <img src="/Carousel/Carousel0.jpg" alt="Carousel Image"/>
+                <SwiperOverlay>
+                    <h2>월 커넥터</h2>
+                    <p>집에서 충전하는 가장 빠른 방법</p>
+                    <button>지금 쇼핑하기</button>
+                </SwiperOverlay>
             </SwiperSlide>
             <SwiperSlide>
                 <img src="/Carousel/Carousel1.jpg" alt="Carousel Image"/>
+                <SwiperOverlay>
+                    <h2>Model Y 사계절 인테리어 매트</h2>
+                    <p>사계절 깨끗한 실내</p>
+                    <button>지금 쇼핑하기</button>
+                </SwiperOverlay>
             </SwiperSlide>
         </Swiper>
     </ShopMainCarouselContainer>
