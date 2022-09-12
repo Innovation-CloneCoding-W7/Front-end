398dcabf60ae9330f23c3a7eaa19f7ad357ed3f8
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-12 11:47:45 +0900

    [Add] 카테고리에 따른 카탈로그 헤더 분기 추가.

--- src/pages/ShopCatalogue/ShopCatalogue.jsx
+++ src/pages/ShopCatalogue/ShopCatalogue.jsx
@@ -6,9 +6,32 @@ import ShopCatalogueBody from "./ShopCatalogueBody";
 
 const ShopCatalogue = () => {
     const {category} = useParams();
+    let title;
+    let footnote;
+    switch (category) {
+        case "charging":
+            title = "충전";
+            footnote = "집에서 충전하기";
+            break;
+        case "vehicle-accessories":
+            title = "차량 액세서리";
+            break;
+        case "apparel":
+            title = "의류";
+            break;
+        case "lifestyle":
+            title = "라이프스타일";
+            break;
+        case "service-installables":
+            title = "서비스 설치";
+            break;
+        default:
+            title = "기타";
+            break;
+    }
     return <ShopLayout>
         <ShopCatalogueContainer>
-            <ShopCatalogueHeader title={"충전"} footnote={"집에서 충전하기"}/>
+            <ShopCatalogueHeader title={title} footnote={footnote}/>
         </ShopCatalogueContainer>
         <ShopCatalogueBody>
 
