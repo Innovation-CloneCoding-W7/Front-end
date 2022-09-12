07eb5f9272aa612afbb54d819aa94ab5dc9683e9
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-12 15:33:10 +0900

    [Add] 빈 카트 페이지 추가.

--- src/pages/ShopCart/styles.jsx
+++ src/pages/ShopCart/styles.jsx
@@ -9,13 +9,76 @@ const ShopCartContainer = styled.div`
 `;
 
 const ShopCartHeaderContainer = styled.div`
-  width: 500px;
+  width: 600px;
+  margin-bottom: 30px;
 
   h1 {
     font-size: 1.5rem;
   }
 `;
 
+const ShopCartBodyEmptyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  width: 500px;
+
+  p {
+    margin-bottom: 10px;
+  }
+
+  div:last-child {
+    position: fixed;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+`;
+
 const ShopCartBodyContainer = styled.div``;
 
-export {ShopCartContainer, ShopCartBodyContainer, ShopCartHeaderContainer};
+const ShopCartBodyEmptyButtons = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 10px;
+  width: 100%;
+
+  a {
+    text-decoration: none;
+    color: inherit;
+    text-align: center;
+    padding: 8px;
+
+    &:first-child {
+      background-color: dodgerblue;
+      color: white;
+      border-radius: 5px;
+      transition: all .3s;
+
+      &:hover {
+        background-color: darkblue;
+      }
+    }
+
+    &:last-child {
+      background-color: white;
+      color: black;
+      border: 2px solid black;
+      border-radius: 5px;
+      transition: all .3s;
+
+      &:hover {
+        background-color: black;
+        color: white;
+      }
+    }
+  }
+`;
+
+export {
+    ShopCartContainer,
+    ShopCartBodyContainer,
+    ShopCartHeaderContainer,
+    ShopCartBodyEmptyContainer,
+    ShopCartBodyEmptyButtons
+};
