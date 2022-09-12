b7f3157ad8fb8674f04cea288786e596e5969184
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-12 10:36:25 +0900

    [Fix] 컨테이너 클릭 시 오버레이 메뉴 슬라이드 아웃 되게 수정.

--- src/components/ShopLayout/ShopLayout.jsx
+++ src/components/ShopLayout/ShopLayout.jsx
@@ -5,9 +5,12 @@ import ShopNavigationBar from "../ShopNavigationBar/ShopNavigationBar";
 
 const ShopLayout = ({children}) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const containerClickHandler = () => {
+        setIsMenuOpen(false);
+    }
     return <GlobalState.Provider value={{isMenuOpen, setIsMenuOpen}}>
         <ShopNavigationBar/>
-        <ShopLayoutContainer $isBlurred={isMenuOpen}>
+        <ShopLayoutContainer $isBlurred={isMenuOpen} onClick={containerClickHandler}>
             {children}
         </ShopLayoutContainer>
     </GlobalState.Provider>;
