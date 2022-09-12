17f8c3414094039554f4e96bbbf4cd97b6cd02d9
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-12 11:52:56 +0900

    [Add] 카탈로그 맵퍼 유틸 추가.

--- src/pages/ShopCatalogue/ShopCatalogue.jsx
+++ src/pages/ShopCatalogue/ShopCatalogue.jsx
@@ -3,39 +3,20 @@ import {useParams} from "react-router-dom";
 import ShopLayout from "../../components/ShopLayout/ShopLayout";
 import ShopCatalogueHeader from "./ShopCatalogueHeader";
 import ShopCatalogueBody from "./ShopCatalogueBody";
+import categoryMapper from "../../utils/categoryMapper";
+import ShopFooter from "../../components/ShopFooter/ShopFooter";
 
 const ShopCatalogue = () => {
     const {category} = useParams();
-    let title;
-    let footnote;
-    switch (category) {
-        case "charging":
-            title = "충전";
-            footnote = "집에서 충전하기";
-            break;
-        case "vehicle-accessories":
-            title = "차량 액세서리";
-            break;
-        case "apparel":
-            title = "의류";
-            break;
-        case "lifestyle":
-            title = "라이프스타일";
-            break;
-        case "service-installables":
-            title = "서비스 설치";
-            break;
-        default:
-            title = "기타";
-            break;
-    }
+    const title = categoryMapper[category];
     return <ShopLayout>
         <ShopCatalogueContainer>
-            <ShopCatalogueHeader title={title} footnote={footnote}/>
+            <ShopCatalogueHeader title={title}/>
         </ShopCatalogueContainer>
         <ShopCatalogueBody>
 
         </ShopCatalogueBody>
+        <ShopFooter/>
     </ShopLayout>
 }
 
