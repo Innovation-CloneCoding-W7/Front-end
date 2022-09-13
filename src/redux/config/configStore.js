49ede6f8459cc94141815a35f315112598f61aa1
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-13 09:47:30 +0900

    [Add] cartSlice.

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import productSliceReducer from "../modules/productSlice";
 import individualProductSliceReducer from "../modules/individualProductSlice"
+import cartSliceReducer from "../modules/cartSlice";
 
 const store = configureStore({
     reducer: {
         productSliceReducer,
-        individualProductSliceReducer
+        individualProductSliceReducer,
+        cartSliceReducer
     },
 });
 
