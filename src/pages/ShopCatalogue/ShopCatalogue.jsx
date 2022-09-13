8f05a2706d5cd600bacd2f0f8c235a8469aa61ef
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-13 10:14:54 +0900

    [Add] 디스패처 연결.

--- src/pages/ShopCatalogue/ShopCatalogue.jsx
+++ src/pages/ShopCatalogue/ShopCatalogue.jsx
@@ -5,9 +5,17 @@ import ShopCatalogueHeader from "./ShopCatalogueHeader";
 import ShopCatalogueBody from "./ShopCatalogueBody";
 import categoryMapper from "../../utils/categoryMapper";
 import ShopFooter from "../../components/ShopFooter/ShopFooter";
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {getProductsThunk} from "../../redux/modules/productSlice";
 
 const ShopCatalogue = () => {
+    const {isLoading, products, error} = useSelector((state) => state.productSliceReducer);
     const {category} = useParams();
+    const dispatcher = useDispatch();
+    useEffect(() => {
+        dispatcher(getProductsThunk(category));
+    }, [dispatcher])
     const title = categoryMapper[category];
     return <ShopLayout>
         <ShopCatalogueContainer>
