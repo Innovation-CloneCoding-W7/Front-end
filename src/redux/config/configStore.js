aebaf6ceea2267cb426cddf2badf6ef2b7cf3992
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-13 09:39:07 +0900

    [Add] getProductSlice & getIndividualProductSlice.

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,7 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
+import productSliceReducer from "../modules/productSlice";
+import individualProductSliceReducer from "../modules/individualProductSlice"
 
 const store = configureStore({
-  reducer: {},
+    reducer: {
+        productSliceReducer,
+        individualProductSliceReducer
+    },
 });
 
 export default store;
