dce3c765ed200b17072c1feabab2b5dc88dd5bbd
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-15 11:56:25 +0900

    [Add] 수량 조절 부분 추가.

--- src/pages/ProductDetail/styles.jsx
+++ src/pages/ProductDetail/styles.jsx
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
 const ProductDetailContainer = styled.div`
-  padding: 30px;
   display: grid;
   grid-template-columns: 67% 30%;
   gap: 3%;
-  padding-top: 80px;
+  padding: 80px 30px 30px 30px;
 `;
 
 const ProductDetailImageContainer = styled.div`
@@ -16,16 +15,43 @@ const ProductDetailImageContainer = styled.div`
 
 const ProductDetailBodyContainer = styled.div`
   h2 {
-    font-size: 1.2rem;
+    font-size: 1.5rem;
     font-weight: normal;
   }
 
   h3 {
     font-size: 1rem;
     font-weight: normal;
+    margin-bottom: 15px;
   }
 `;
 
-const Quantity = styled.div``;
+const Quantity = styled.div`
+  & > p {
+    font-size: 0.8rem;
+    margin-bottom: 10px;
+  }
+`;
 
-export {Quantity, ProductDetailBodyContainer, ProductDetailContainer, ProductDetailImageContainer}
+const QuantityInput = styled.div`
+  display: flex;
+  align-items: center;
+
+  p {
+    font-weight: bold;
+
+    &.add, &.subtract {
+      cursor: pointer;
+    }
+
+    &.subtract {
+      padding-right: 15px;
+    }
+
+    &.add {
+      padding-left: 15px;
+    }
+  }
+`;
+
+export {Quantity, ProductDetailBodyContainer, ProductDetailContainer, ProductDetailImageContainer, QuantityInput}
